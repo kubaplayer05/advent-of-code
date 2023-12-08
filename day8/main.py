a5b9ee4bbe20f5f8683ec952784bb20abf3fab68
Jakub Grzybek <jakubgrzybekinf@gmail.com>
2023-12-08 22:35:12 +0100

    day 8 / part 2

--- day8/main.py
+++ day8/main.py
@@ -1,9 +1,10 @@
 class NetworkNode:
 
-    def __init__(self, name: str, left: str, right: str):
+    def __init__(self, name: str, left: str, right: str, index: int):
         self.name = name
         self.left = left
         self.right = right
+        self.index = index
 
 
 def read_lines(path):
@@ -19,12 +20,12 @@ def read_lines(path):
 def prepare_nodes(nodes_arr: list[str]):
     nodes = []
 
-    for node in nodes_arr:
+    for i, node in enumerate(nodes_arr):
         name = node.split("=")[0].strip()
         rl = node.split("=")[1].strip()
         left = rl.split(",")[0].removeprefix("(").strip()
         right = rl.split(",")[1].removesuffix(")").strip()
-        nodes.append(NetworkNode(name, left, right))
+        nodes.append(NetworkNode(name, left, right, i))
 
     return nodes
 
@@ -37,14 +38,24 @@ def find_node(nodes: list[NetworkNode], node_name: str):
     return -1
 
 
-def get_steps_count(sequence: str, nodes: list[NetworkNode]):
-    actual_node = "AAA"
-    actual_index = find_node(nodes, actual_node)
+def find_starting_nodes(nodes: list[NetworkNode]):
+    starting_nodes = []
+
+    for node in nodes:
+        if node.name.endswith("A"):
+            starting_nodes.append(node)
+
+    return starting_nodes
+
+
+def get_steps_count(starting_node: NetworkNode, sequence: str, nodes: list[NetworkNode]):
+    actual_node = starting_node.name
+    actual_index = starting_node.index
     steps = 0
     sequence_index = 0
     sequence_length = len(sequence)
 
-    while actual_node != "ZZZ":
+    while actual_node.endswith("Z") is False:
         steps += 1
         move = sequence[sequence_index]
 
@@ -64,14 +75,41 @@ def get_steps_count(sequence: str, nodes: list[NetworkNode]):
     return steps
 
 
+def get_steps_lmc(steps: list[int]):
+    steps_lmc = steps[0]
+
+    for i in range(1, len(steps)):
+        num1 = steps_lmc
+        num2 = steps[i]
+
+        while num2 != 0:
+            temp = num2
+            num2 = num1 % num2
+            num1 = temp
+        gcd = num1
+        steps_lmc = (steps_lmc * steps[i]) // gcd
+
+    return steps_lmc
+
+
 def main():
     lines = read_lines("./input.txt")
     sequence = lines[0]
     nodes_str = lines[1:]
     nodes = prepare_nodes(nodes_str)
-    steps = get_steps_count(sequence, nodes)
 
-    print(steps)
+    starting_nodes = find_starting_nodes(nodes)
+    nodes_steps = []
+
+    for node in starting_nodes:
+        step = get_steps_count(node, sequence, nodes)
+        nodes_steps.append(step)
+
+    print(nodes_steps)
+
+    lmc = get_steps_lmc(nodes_steps)
+
+    print(lmc)
 
 
 if __name__ == "__main__":
